affd14b2041b0a4a4c30afcb96690ea66c13b177
vldzgr123 <vlad.z.00@inbox.ru>
2023-04-02 13:47:31 +0600

    floyd

---
+++ Classlab/laba4list/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите кол-во вершин:");
+int n=Int32.Parse(Console.ReadLine());
+double [,] matr=new double[n,n];
+Console.WriteLine("Ввод элементов матрицы:");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++){
+        matr[i,j]=Int32.Parse(Console.ReadLine());
+        if (matr[i,j]==0){
+            matr[i,j]=double.PositiveInfinity;
+        }
+        if(i==j){
+            matr[i,j]=0;
+        }
+    }
+}
+for (int k=0;k<n;k++){
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            matr[i,j]=Math.Min(matr[i,k]+matr[k,j],matr[i,j]);
+        }
+    }
+}
+for (int i=0;i<n;i++){
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{matr[i,j]}\t");
+        
+    }
+    Console.WriteLine("");
+}
