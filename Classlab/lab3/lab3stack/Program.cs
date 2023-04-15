9506e3d51f491f154ee9e0115e18143f72d92ddd
vldzgr123 <vlad.z.00@inbox.ru>
2023-04-15 13:50:40 +0600

    1

--- Classlab/lab3/lab3stack/Program.cs
+++ Classlab/lab3/lab3stack/Program.cs
@@ -117,3 +117,112 @@ static void menu(){
     Console.WriteLine("7.Вывод Массива");
     Console.WriteLine("8.Выход");
 }
+static void mainmenu(){
+    var stck = new Stack<string>();
+Console.WriteLine("Введите элементы:");
+while (true)
+{
+    string? n =Console.ReadLine();
+    if (n=="") break;
+    stck.Push(n);
+}
+Console.Clear();
+while(true){
+    menu();
+    int n=Convert.ToInt32(Console.ReadLine());
+    if (n==8)
+        break;
+    if (n>0 && n<9){
+        switch(n){
+        case 1://Push1
+            Console.Clear();
+            Console.WriteLine("Введите элемент, который нужно добавить:");
+            string ff=Console.ReadLine();
+            Console.WriteLine("Массив до:");
+            foreach(var f in stck){
+                Console.Write(f+" ");
+            }
+            stck.Push(ff);
+            Console.WriteLine("");
+            Console.WriteLine("Массив после:");
+            foreach(var f in stck){
+                Console.Write(f+" ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("=======================================================");
+            break;
+        case 2://Pop1
+            Console.Clear();
+            Console.WriteLine("Массив до:");
+            foreach(var f in stck){
+                Console.Write(f+" ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Элемент:");
+            Console.WriteLine(stck.Pop());
+            Console.WriteLine("Массив после:");
+            foreach(var f in stck){
+                Console.Write(f+" ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("=======================================================");
+            break;
+        case 3://Peek1
+            Console.Clear();
+            Console.WriteLine("Массив до:");
+            foreach(var f in stck){
+                Console.Write(f+" ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Элемент:");
+            Console.WriteLine(stck.Peek());
+            Console.WriteLine("Массив после:");
+            foreach(var f in stck){
+                Console.Write(f+" ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("=======================================================");
+            break;
+        case 4://Contains1
+            Console.Clear();
+            Console.WriteLine("Введите элемент:");
+            Console.WriteLine(stck.Contains(Console.ReadLine()));
+            Console.WriteLine("=======================================================");
+            break;
+        case 5://Clear1
+            Console.Clear();
+            Console.WriteLine("Массив до:");
+            foreach(var f in stck){
+                Console.Write(f+" ");
+            }
+            Console.WriteLine("");
+            stck.Clear();
+            Console.WriteLine("Массив после:");
+            foreach(var f in stck){
+                Console.Write(f+" ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("=======================================================");
+            break;
+        case 6:
+            Console.Clear();
+            Console.WriteLine("Количество элементов:");
+            Console.WriteLine(stck.Count);
+            Console.WriteLine("=======================================================");
+            break;
+        case 7:
+            Console.Clear();
+            foreach(var f in stck){
+                Console.Write(f+" ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("=======================================================");
+            Console.WriteLine("");
+            break;
+        }
+    }
+    else{
+        Console.WriteLine("Ошибка!");
+    }
+}
+}
