bc7fae44c003953a46892fdc7f9fc8628fa96120
vldzgr123 <vlad.z.00@inbox.ru>
2023-04-23 13:53:31 +0600

    1

---
+++ Classlab/lab4delegate/Program.cs
@@ -0,0 +1,58 @@
+﻿
+Console.WriteLine("Введите первое число:");
+int x=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int y=Convert.ToInt32(Console.ReadLine());
+while(true){
+    Console.WriteLine("1.Сложение");
+    Console.WriteLine("2.Вычитание");
+    Console.WriteLine("3.Умножение");
+    Console.WriteLine("4.Деление");
+    Console.WriteLine("5.Выход");
+    Mathoper summ=IMath.sum;
+    Mathoper subb=IMath.sub;
+    Mathoper multt=IMath.mult;
+    Mathoper divv=IMath.div;
+    int n=Convert.ToInt32(Console.ReadLine());
+    if (n==5) break;
+    switch(n){
+        case 1:
+            Console.Clear();
+            Console.WriteLine($"Результат сложение: {summ(x,y)}");
+            Console.WriteLine("=======================================");
+            break;
+        case 2:
+            Console.Clear();
+            Console.WriteLine($"Результат вычитания: {subb(x,y)}");
+            Console.WriteLine("=======================================");
+            break;
+        case 3:
+            Console.Clear();
+            Console.WriteLine($"Результат умножения: {multt(x,y)}");
+            Console.WriteLine("=======================================");
+            break;
+        case 4:
+            Console.Clear();
+            Console.WriteLine($"Результат деления: {divv(x,y)}");
+            Console.WriteLine("=======================================");
+            break;
+    }
+}
+interface IMath{
+
+    static int x;
+    static int y;
+    static int sum(int x,int y){
+        return x+y;
+    }
+    static int mult(int x,int y){
+        return x*y;
+    }
+    static int div(int x,int y){
+        return x/y;
+    }
+    static int sub(int x,int y){
+        return x-y;
+    }
+}
+public delegate int Mathoper(int x , int y);
---
+++ Classlab/lab5delegatelymbda/Program.cs
@@ -0,0 +1,50 @@
+﻿Operation sredarif =(x,y,z) => (x+y+z)/3;
+Operation min =(x,y,z) => Math.Min(Math.Min(x,y),Math.Min(y,z));
+Operation max =(x,y,z) => Math.Max(Math.Max(x,y),Math.Max(y,z));
+Operation sum =(x,y,z) => x+y+z;
+Operation mult =(x,y,z) => x*y*z;
+
+Console.WriteLine("Введите первое число:");
+double x=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+double y=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+double z=Convert.ToInt32(Console.ReadLine());
+while(true){
+    Console.WriteLine("1.Сложение");
+    Console.WriteLine("2.Среднее арифметическое");
+    Console.WriteLine("3.Умножение");
+    Console.WriteLine("4.Минимальное");
+    Console.WriteLine("5.Максимальное");
+    Console.WriteLine("6.Выход");
+    int n=Convert.ToInt32(Console.ReadLine());
+    if (n==6) break;
+    switch(n){
+        case 1:
+            Console.Clear();
+            Console.WriteLine($"Результат сложение: {sum(x,y,z)}");
+            Console.WriteLine("=======================================");
+            break;
+        case 2:
+            Console.Clear();
+            Console.WriteLine($"Среднее арифметическое: {sredarif(x,y,z)}");
+            Console.WriteLine("=======================================");
+            break;
+        case 3:
+            Console.Clear();
+            Console.WriteLine($"Результат умножения: {mult(x,y,z)}");
+            Console.WriteLine("=======================================");
+            break;
+        case 4:
+            Console.Clear();
+            Console.WriteLine($"Минимальное: {min(x,y,z)}");
+            Console.WriteLine("=======================================");
+            break;
+        case 5:
+            Console.Clear();
+            Console.WriteLine($"Максимальное: {max(x,y,z)}");
+            Console.WriteLine("=======================================");
+            break;
+    }
+}
+delegate double Operation(double x,double y,double z);
