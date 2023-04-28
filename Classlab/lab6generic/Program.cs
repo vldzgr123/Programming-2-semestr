f38f5efe6a01e2ea76908e27dfec80fd02a2404b
vldzgr123 <vlad.z.00@inbox.ru>
2023-04-28 10:47:01 +0600

    generic

---
+++ Classlab/lab6generic/Program.cs
@@ -0,0 +1,37 @@
+﻿Example<int> example=new Example<int>();
+example.Add(1);
+example.Add(2);
+example.Rem(2);
+class Example<T>{
+    private T[] mass=Array.Empty<T>();
+    public T this[int index]{
+        get{
+            return mass[index];
+        }
+        set{
+            mass[index]=value;
+        }
+    }
+    public void Add(T value){
+        var newMass=new T[mass.Length+1];
+        for (int i =0; i <newMass.Length; i++)
+        {
+            newMass[i]=mass[i];
+        }
+        newMass[mass.Length]=value;
+        mass=newMass;
+    }
+    public void Rem(int value){
+        var newMass=new T[mass.Length-1];
+        int k=0;
+        for (int i =0; i <newMass.Length; i++)
+        {
+            if (i!=value){
+                newMass[k]=mass[i];
+                k++;
+            }
+        }
+        mass=newMass;
+    }
+   
+}
