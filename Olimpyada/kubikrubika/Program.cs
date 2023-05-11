c30162455c4ecbd43689dd1c5cb6d42902034956
vldzgr123 <vlad.z.00@inbox.ru>
2023-05-11 09:29:00 +0600

    lab3

---
+++ Olimpyada/kubikrubika/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Кол-во элементов и кол-во вращений:");
+string str=Console.ReadLine();
+int N=Convert.ToInt32(str.Split(" ")[0]);
+int M=Convert.ToInt32(str.Split(" ")[1]);
+Console.WriteLine("Начальный координаты:");
+str=Console.ReadLine();
+int XN=Convert.ToInt32(str.Split(" ")[0]);
+int YN=Convert.ToInt32(str.Split(" ")[0]);
+int ZN=Convert.ToInt32(str.Split(" ")[0]);
+for (int i = 0; i < N; i++){
+    int XK,YK,ZK;
+    string A=Console.ReadLine();
+    string K=Console.ReadLine();
+    string S=Console.ReadLine();
+    if (S=="1"){
+        
+    }
+    else{
+
+    }
+}
