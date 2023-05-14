ed5636a92ed834f4a5ff1208e55b5b89268fe217
vldzgr123 <vlad.z.00@inbox.ru>
2023-05-15 02:30:36 +0600

    1

--- dmat/deikstra/Program.cs
+++ dmat/deikstra/Program.cs
@@ -1,120 +1,65 @@
-﻿
-List<versh> vershini=new List<versh>();
-List<smeg> rebra=new List<smeg>();
-string vers="";
-Console.WriteLine("Введите вершины:");
+﻿List<Rebro> rebra=new List<Rebro>();
+string vershini="";
+int count=0;
 while(true){
-    versh ver=new versh();
-    ver.name=Console.ReadLine();
-    if (ver.name=="") break;
-    vershini.Add(ver);
+    string reb1=Console.ReadLine();
+    string[] reb=reb1.Split(" ");
+    var rebro=new Rebro();
+    if (reb1=="") break;
+    if (!vershini.Contains(reb[0])) vershini+=reb[0];
+    if (!vershini.Contains(reb[1])) vershini+=reb[1];
+    rebro.ver1=reb[0];
+    rebro.ver2=reb[1];
+    rebro.ves=Convert.ToInt32(reb[2]);
+    count=Math.Max(count,Convert.ToInt32(reb[2]));
+    rebra.Add(rebro);
 }
-Console.WriteLine("Введите ребра:");
-StreamReader str2=new StreamReader("1.txt");
-while(true){
-    smeg s=new smeg();
-    string? str1=str2.ReadLine();
-    if (str1=="0") break;
-    string[] ans= str1.Split(" ");
-    string ver1=ans[0];
-    string ver2=ans[1];
-    if (!vers.Contains(ver1)||!vers.Contains(ver2)){
-        if(!vers.Contains(ver2)&&!vers.Contains(ver1)){
-            foreach(versh i in vershini){
-                if (i.name==ver2){
-                    s.ver2=i;
-                    vers+=i.name;
-                }
-                if (i.name==ver1){
-                    s.ver1=i;
-                    vers+=i.name;
-                }
-            }
-        }
-        if(!vers.Contains(ver1)&&vers.Contains(ver2)){
-            foreach(versh i in vershini){
-                if (i.name==ver1){
-                    s.ver1=i;
-                    vers+=i.name;
-                    break;
-                }
-            }
-        }
-        if(!vers.Contains(ver2)&&vers.Contains(ver1)){
-            foreach(versh i in vershini){
-                if (i.name==ver2){
-                    s.ver2=i;
-                    vers+=i.name;
-                    break;
-                }
-            }
-        }
-    }
-    s.ves=Convert.ToDouble(ans[2]);
-    rebra.Add(s);
+var valuever=new int[vershini.Length]; 
+for (int i = 0; i < valuever.Length; i++)
+{
+    valuever[i]=1*count;
 }
-str2.Close();
-Console.WriteLine("Введите от какой вершины:");
-string ot=Console.ReadLine();
-Console.WriteLine("Введите до какой вершины");
-string to=Console.ReadLine();
-Algorithm(rebra,vershini,ot,to);
-
-static void Algorithm(List<smeg> rebra, List<versh> vershini, string ot, string to){
-    int toid=0;
-    double put=0;
-    for(int i=0;i<vershini.Count;i++)
-    {
-        if(i==0){
-            for(int k=0;k<vershini.Count;k++){
-                if (vershini[k].name==ot){
-                    vershini[k].minput=0;
-                    vershini[k].check=true;
-                }
-                if (vershini[k].name==to){
-                    toid=k;
-                }
-            }
-        }
-        else{
-            for(int k=0;k<rebra.Count;k++){
-                if (ot.Contains(rebra[k].ver1.name) || ot.Contains(rebra[k].ver2.name)){
-                    if(ot.Contains(rebra[k].ver1.name)){
-                        if (rebra[k].ver2.check==false) rebra[k].ver2.minput=Math.Min(rebra[k].ver2.minput,rebra[k].ves+rebra[k].ver1.minput);
-                    }
-                    else{
-                        if (rebra[k].ver2.check==false) rebra[k].ver1.minput=Math.Min(rebra[k].ver1.minput,rebra[k].ves+rebra[k].ver2.minput);
-                    }
-                }
-            }
-            double tmp=double.PositiveInfinity;
-            int minid=0;
-            for (int k=0;k<vershini.Count;k++){
-                if (tmp>vershini[k].minput && vershini[k].check==false){
-                    minid=k;
-                    tmp=vershini[k].minput;
-                }
-            }
-            if (vershini[minid].name==to){
-                Console.WriteLine(vershini[minid].minput);
-                break;
-            }
-            else{
-                vershini[minid].check=true;
-                ot=vershini[minid].name;
-            }
+int ot=Convert.ToInt32(Console.ReadLine())-1;
+int too=Convert.ToInt32(Console.ReadLine())-1;
+string start=Convert.ToString(ot+1);
+for (int i=0;i<valuever.Length;i++){
+    for(int j=0;j<rebra.Count;j++){
+        if(rebra[j].ver1==Convert.ToString(ot+1) && rebra[j].ver2==Convert.ToString(i+1)){
+            valuever[i]=rebra[j].ves;
         }
     }
 }
-class smeg
-{
-    public versh ver1;
-    public versh ver2;
-    public double ves;
+int minimumValue=count;
+int minVer=0;
+for (int i=0;i<valuever.Length;i++){
+    if (valuever[i]<minimumValue && !start.Contains(Convert.ToString(i+1))){
+        minimumValue=valuever[i];
+        minVer=i;
+    }
 }
-class versh
-{
-    public string name;
-    public double minput=double.PositiveInfinity;
-    public bool check=false;
+ot=minVer;
+while (ot!=0){
+    start+=Convert.ToString(ot+1);
+    for(int i=0;i<valuever.Length;i++){
+        for(int j=0;j<rebra.Count;j++){
+            if(rebra[j].ver1==Convert.ToString(ot+1) &&rebra[j].ver2==Convert.ToString(i+1)){
+                valuever[i]=Math.Min(rebra[j].ves+valuever[ot],valuever[i]);
+            }
+        }
+    }
+    minimumValue=count;
+    minVer=0;
+    for (int i=0;i<valuever.Length;i++){
+        if(valuever[i]<minimumValue && !start.Contains(Convert.ToString(i+1))){
+            minimumValue=valuever[i];
+            minVer=i;
+        }
+    }
+    ot=minVer;
+}
+Console.WriteLine(valuever[too]);
+class Rebro{
+    public string ver1;
+    public string ver2;
+    public int ves;
 }
---
+++ dmat/ford/Program.cs
@@ -0,0 +1,51 @@
+﻿﻿double[,] array = new double[5, 5] {
+{double.PositiveInfinity, 1.0, double.PositiveInfinity, double.PositiveInfinity, 3.0 },
+{double.PositiveInfinity, double.PositiveInfinity, 8.0, 7.0,1.0},
+{double.PositiveInfinity, double.PositiveInfinity, 8.0, 7.0, 1.0 },
+{double.PositiveInfinity, double.PositiveInfinity,2.0, double.PositiveInfinity, double.PositiveInfinity },
+{double.PositiveInfinity,double.PositiveInfinity, double.PositiveInfinity, 4.0, double.PositiveInfinity }};
+
+int ot = Convert.ToInt32(Console.ReadLine()) - 1;
+
+
+
+double[,] matrix = new double[5, 5];
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        if (i != ot)
+        {
+            matrix[i, j] = double.PositiveInfinity;
+        }
+        else
+        {
+            matrix[i, j] = 0;
+        }
+    }
+}
+for (int k = 1; k < 5; k++)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (i == ot)
+        {
+            matrix[i, k] = 0;
+        }
+        else
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                matrix[i, k] = double.Min(matrix[i, k], matrix[j, k - 1] + array[j, i]);
+            }
+        }
+    }
+}
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
